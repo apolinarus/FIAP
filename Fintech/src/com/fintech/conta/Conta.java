c9b921fc3775bab2dee197f10c76a23be886b9e8
apolinarus <robsonat@gmail.com>
2022-08-31 19:11:33 -0300

    Insert attributes on Conta Class.

--- Fintech/src/com/fintech/conta/Conta.java
+++ Fintech/src/com/fintech/conta/Conta.java
@@ -1,5 +1,44 @@
 package com.fintech.conta;
 
 public class Conta {
-
+	
+	//Atributos
+	private int numero;
+	private int agencia;
+	private double saldo;
+	
+	
+	//Construtores
+	public Conta(){
+		
+	}
+	
+	public Conta(int numero, int agencia, double saldo){
+		this.numero = numero;
+		this.agencia = agencia;
+		this.saldo = saldo;
+	}
+	
+	
+	//Metodos
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public int getAgencia() {
+		return agencia;
+	}
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void depositar(double saldo) {
+		this.saldo += saldo;
+	}
+	
+	
 }
--- Fintech/src/com/fintech/conta/TesteConta.java
+++ Fintech/src/com/fintech/conta/TesteConta.java
@@ -1,5 +1,14 @@
 package com.fintech.conta;
 
 public class TesteConta {
+	
+	public static void main(String[] args) {
+		
+		//Instancia um objeto da classe Conta
+		Conta cc = new Conta(111, 222, 5000);
+		
+		//Exibe o número da agência no console.
+		System.out.println(cc.getAgencia()); 
+	}
 
 }
